861e7f28a998ada1e9a27a788691bd503a2b36e0
Samuel Rohrer <srohrer32@gmail.com>
2016-10-03 23:37:04 -0400

    able to ping and reset IMST using makeLoRaPacket

--- imst/configIMST.js
+++ imst/configIMST.js
@@ -35,7 +35,7 @@ const DEVMGMT_STATUS_OK = 0x00;                // Operation successful;
 const DEVMGMT_STATUS_ERROR = 0x01;             // Operation failed;
 const DEVMGMT_STATUS_CMD_NOT_SUPPORTED = 0x02; // Command is not supported;
 const DEVMGMT_STATUS_WRONG_PARAMETER = 0x03;
-
+// CRC constants
 const CRC16_INIT_VALUE = 0xFFFF;
 const CRC16_GOOD_VALUE = 0x0F47;
 const CRC16_POLYNOM = 0x8408;
@@ -45,43 +45,38 @@ var slip = require('slip');
 var SerialPort = require('serialport');
 
 // start the slip decoder
-var logMsg = function(msg) { console.log("A SLIP message received!"); };
+var logMsg = function(msg) { console.log('A SLIP message received: ' + msg); };
 var decoder = new slip.Decoder({onMessage : logMsg});
 
 // open the serial port
 var port = new SerialPort('/dev/ttyUSB0',
                           {baudrate : 115200, parser : SerialPort.parsers.raw});
 
+// start listening on the serial port
+var currData;
+port.on('data', function(data) { currData = decoder.decode(data); });
+
+// call configIMST
+configIMST(port, 1);
+
 // function to configIMST
-function configIMST(port, decoder, endpointID) {
+function configIMST(port, endpointID) {
   // ping the IMST to make sure alive
-  // assuming endpointID=1, checksum = 22,7
-  var ping_msg = new Uint8Array([ endpointID, DEVMGMT_MSG_PING_REQ, 22, 7 ]);
+  makePacket(endpointID, DEVMGMT_MSG_PING_REQ, '', port);
 
   // reset the IMST for fresh settings (takes 200 ms)
-  // assuming endpointID=1, checksum=32,98
-  var reset_msg = new Uint8Array([ endpointID, DEVMGMT_MSG_RESET_REQ, 32, 98 ]);
+//  makePacket(endpointID, DEVMGMT_MSG_PING_REQ, '', port);
 
   // configure the ISMT
-  var config_msg = new Uint8Array([
-    endpointID, DEVMGMT_MSG_SET_RADIO_CONFIG_REQ,
-    0,          16,
-    16,         1,
-    1,          0,
-    0,          192,
-    228,        0,
-    0,          1,
-    5,          0,
-    1,          1000,
-    15,         15
-  ]);
+  var config_msg = new Uint8Array(
+      [ 0, 16, 16, 1, 1, 0, 0, 192, 228, 0, 0, 1, 5, 0, 1, 1000, 15, 15 ]);
+  makePacket(endpointID, DEVMGMT_MSG_SET_RADIO_CONFIG_REQ, config_msg, port);
 }
 
-makePacket(1, 1, '');
 // function to make lora packet and send
-function makePacket(endpointID, msgID, message) {
+function makePacket(endpointID, msgID, message, port) {
   // declare the packet
-  const packet = new Uint8Array(message.length + 5);
+  const packet = new Uint8Array(message.length + 7);
   packet[0] = 0xC0;
   packet[1] = endpointID;
   packet[2] = msgID;
@@ -90,13 +85,22 @@ function makePacket(endpointID, msgID, message) {
     packet[3 + i] = message[i];
   }
   var result = CRC16_Calc(packet, 1, 2 + message.length, CRC16_INIT_VALUE);
-  result = (~result & 65535);
   packet[3 + message.length] = result & 0xFF;
   packet[3 + message.length + 1] = (result >> 8);
-  var check = CRC16_Check(packet, 1, 2 + message.length + 2, CRC16_INIT_VALUE);
-  console.log('Check is ' + check);
+  var check = CRC16_Check(packet, 1, 4 + message.length, CRC16_INIT_VALUE);
+
+  // check that checksum correct before adding final C0
+  if (check) {
+    packet[3 + message.length + 2] = 0xC0;
+    packet = slip.encode(packet);
+    port.on('open', function() { port.write(packet); });
+  } else {
+    console.log('Checksum check failed! Your message: ' + message +
+                'will not be delivered');
+  }
 }
 
+// --------------- CRC FUNCTIONS ------------------------------>
 // CRC calculation
 function CRC16_Calc(data, start, length, initVal) {
   // init crc
@@ -115,7 +119,7 @@ function CRC16_Calc(data, start, length, initVal) {
       byte >>= 1;
     }
   }
-  return crc;
+  return (~crc & 65535);
 }
 
 //------------------------------------------------------------------------------
@@ -140,8 +144,8 @@ function CRC16_Calc(data, start, length, initVal) {
 //------------------------------------------------------------------------------
 function CRC16_Check(data, start, length, initVal) {
   // calculate ones complement of CRC16
-  var crc = ~CRC16_Calc(data, start, length, initVal);
-  if ((crc & 65535) == CRC16_GOOD_VALUE)
+  var crc = CRC16_Calc(data, start, length, initVal);
+  if (crc == CRC16_GOOD_VALUE)
     return true;
   return false;
 }
